e2ff3dcbb617f902ab722b02abf42e006dfac4b4
Barbarubia <dnl.desimone@gmail.com>
2022-03-29 16:03:03 +0200

    Milestone1 migliorata stampa risultato

--- src/components/CardTitolo.vue
+++ src/components/CardTitolo.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-    <p>Titolo: {{ titoloData.title }}</p>
-    <p>Titolo Originale: {{ titoloData.original_title }}</p>
-    <p>Lingua: {{ titoloData.original_language }}</p>
-    <p>Voto: {{ titoloData.vote_average }}</p>
-  </div>
+  <ul>
+    <li>
+      <p>Titolo: {{ titoloData.title }}</p>
+      <p>Titolo Originale: {{ titoloData.original_title }}</p>
+      <p>Lingua: {{ titoloData.original_language }}</p>
+      <p>Voto: {{ titoloData.vote_average }}</p>
+    </li>
+  </ul>
 </template>
 
 <script>
