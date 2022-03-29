ba7b98e97934ce85130813b75864ca6f9d5c09fe
Barbarubia <dnl.desimone@gmail.com>
2022-03-29 17:52:01 +0200

    Milestone 2 bandierine accanto alla lingua

--- src/components/CardTitolo.vue
+++ src/components/CardTitolo.vue
@@ -3,7 +3,7 @@
     <li>
       <p>Titolo: {{ titoloData.title }}</p>
       <p>Titolo Originale: {{ titoloData.original_title }}</p>
-      <p>Lingua: {{ titoloData.original_language }}</p>
+      <p>Lingua: {{ titoloData.original_language }} <img :src="findFlag()" :alt="titoloData.original_language"></p>
       <p>Voto: {{ titoloData.vote_average }}</p>
     </li>
   </ul>
@@ -14,10 +14,21 @@ export default {
   name: 'CardTitolo',
   props: {
     titoloData: Object
+  },
+  methods: {
+    findFlag () {
+      if (this.titoloData.original_language === 'de' || this.titoloData.original_language === 'en' || this.titoloData.original_language === 'es' || this.titoloData.original_language === 'fr' || this.titoloData.original_language === 'it') {
+        return require('../assets/img/flags/' + this.titoloData.original_language + '.png')
+      } else {
+        return require('../assets/img/flags/unknown.png')
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+img {
+  height: 1rem;
+}
 </style>
