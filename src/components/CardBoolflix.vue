7b951b42b5575b84537b74e7338249c5a9ee18d1
Barbarubia <dnl.desimone@gmail.com>
2022-03-30 22:31:22 +0200

    unificato componente Card sia x film che x serie

---
+++ src/components/CardBoolflix.vue
@@ -0,0 +1,101 @@
+<template>
+  <ul>
+    <li class="card-titolo">
+      <div class="poster">
+        <img :src="findPoster()" :alt="ricercaData.title ? ricercaData.title : ricercaData.name">
+        <h4 class="no-poster">{{ ricercaData.title ? ricercaData.title : ricercaData.name }}</h4>
+
+      </div>
+      <div class="info-titolo">
+        <h4>{{ ricercaData.title ? ricercaData.title : ricercaData.name }}</h4>
+        <h5>(Original: {{ ricercaData.original_title ? ricercaData.original_title : ricercaData.original_name }})</h5>
+        <img class="img-flag" :src="findFlag()" :alt="ricercaData.original_language">
+        <div class="stars">
+          <span v-if="ratingFive(ricercaData.vote_average) > 0"><font-awesome-icon v-for="counter in ratingFive(ricercaData.vote_average)" :key="counter" icon="fa-solid fa-star" /></span>
+          <span v-if="ratingFive(ricercaData.vote_average) < 5"><font-awesome-icon v-for="counter in (5 - ratingFive(ricercaData.vote_average))" :key="counter" icon="fa-regular fa-star" /></span>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CardBoolflix',
+  props: {
+    ricercaData: Object
+  },
+  methods: {
+    findFlag () {
+      if (this.ricercaData.original_language === 'de' || this.ricercaData.original_language === 'en' || this.ricercaData.original_language === 'es' || this.ricercaData.original_language === 'fr' || this.ricercaData.original_language === 'it') {
+        return require('../assets/img/flags/' + this.ricercaData.original_language + '.png')
+      } else {
+        return require('../assets/img/flags/unknown.png')
+      }
+    },
+    findPoster () {
+      if (this.ricercaData.poster_path) {
+        return 'https://image.tmdb.org/t/p/w342' + this.ricercaData.poster_path
+      } else {
+        return require('../assets/img/blank_poster.jpg')
+      }
+    },
+    ratingFive (vote) {
+      return Math.round(vote / 2)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+  display: inline-block;
+  margin: 0 .5rem;
+  width: calc(100% / 5 - 1rem);
+}
+
+.card-titolo {
+  position: relative;
+  text-align: center;
+  .info-titolo {
+    opacity: 0;
+    background-color: rgba(0, 0, 0, 0.6);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    flex-direction: column;
+    color: white;
+  }
+  &:hover {
+    .info-titolo {
+      opacity: 1;
+    }
+  }
+}
+
+.poster img{
+  width: 100%;
+}
+
+.no-poster {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+  padding: .5rem 2rem;
+  color: white;
+}
+
+.img-flag {
+  height: 2rem;
+}
+
+.stars {
+  color: yellow;
+}
+</style>
--- src/components/MainBoolflix.vue
+++ src/components/MainBoolflix.vue
@@ -2,30 +2,28 @@
   <main>
     <div class="container">
       <h2 v-if="arrayMovies.length != 0">Film</h2>
-      <card-movie
+      <card-boolflix
         v-for="movie in arrayMovies"
         :key="movie.it"
-        :movie-data="movie"
+        :ricerca-data="movie"
       />
       <h2 v-if="arraySeries.length != 0">Serie TV</h2>
-      <card-serie
+      <card-boolflix
         v-for="serie in arraySeries"
         :key="serie.it"
-        :serie-data="serie"
+        :ricerca-data="serie"
       />
     </div>
   </main>
 </template>
 
 <script>
-import CardMovie from './CardMovie.vue'
-import CardSerie from './CardSerie.vue'
+import CardBoolflix from './CardBoolflix.vue'
 
 export default {
   name: 'MainBoolflix',
   components: {
-    CardMovie,
-    CardSerie
+    CardBoolflix
   },
   props: {
     arrayMovies: Array,
